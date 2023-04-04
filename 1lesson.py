1af5dd0f8d98c8b4ea557fae43b332f0d981fc59
AbramQA <abramov@bolid.ru>
2023-04-04 16:51:43 +0300

    update

--- 1lesson.py
+++ 1lesson.py
@@ -1,2 +1,6 @@
-print("Hello, world")
-print("World, hello")
+a = input().lower()
+# print("Аденин: " + str(a.count("а")))
+# print("Гуанин: " + str(a.count("г")))
+# print("Цитозин: " + str(a.count("ц")))
+# print("Тимин: " + str(a.count("т")))
+print(f"Аденин: {a.count('а')}")
